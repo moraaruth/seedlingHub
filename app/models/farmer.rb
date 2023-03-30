6240f425a935ce1969e5c24cb2dcd37cfc7baf74
Ruth Moraa <moraar37@gmail.com>
2023-03-30 06:03:03 +0300

    adding validations to models

--- app/models/farmer.rb
+++ app/models/farmer.rb
@@ -1,2 +1,6 @@
 class Farmer < ApplicationRecord
+    validates :username, presence: true, uniqueness: true
+    validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+    validates :password, presence: true, length: { minimum: 6 }
+    has_secure_password
 end
--- app/models/seedling.rb
+++ app/models/seedling.rb
@@ -1,2 +1,5 @@
 class Seedling < ApplicationRecord
+    validates :name, presence: true, uniqueness: true
+    validates :price, numericality: { greater_than_or_equal_to: 0 }
+    validates :description, length: { maximum: 500 }
 end
